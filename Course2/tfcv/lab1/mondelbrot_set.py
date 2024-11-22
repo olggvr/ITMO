dd3cbb336795b03f46e5f8b7fa3661f497df02e7
Cucumber090-1 <gavroleg7@gmail.com>
2024-11-22 17:06:39 +0300

    visualize mondelbrot set

---
+++ Course2/tfcv/lab1/mondelbrot_set.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+width, height = 800, 800
+x_min, x_max = -2.5, 1.5
+y_min, y_max = -2.0, 2.0
+max_iter = 100
+
+def mandelbrot(c, max_iter):
+
+    z = 0
+    for n in range(max_iter):
+        z = z * z + c
+        if abs(z) > 2:
+            return n
+    return max_iter
+
+x, y = np.linspace(x_min, x_max, width), np.linspace(y_min, y_max, height)
+X, Y = np.meshgrid(x, y)
+C = X + 1j * Y
+
+Z = np.zeros(C.shape, dtype=int)
+for i in range(height):
+    for j in range(width):
+        Z[i, j] = mandelbrot(C[i, j], max_iter)
+
+
+plt.figure(figsize=(10, 10))
+plt.imshow(Z, extent=[x_min, x_max, y_min, y_max], cmap='hot', origin='lower')
+plt.colorbar(label='Количество итераций до расходимости')
+plt.title("Множество Мандельброта")
+plt.xlabel("Re(c)")
+plt.ylabel("Im(c)")
+plt.show()

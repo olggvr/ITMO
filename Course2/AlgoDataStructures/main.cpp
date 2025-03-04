7ef259980bb4b73d0160107b6052b9e068ce0e7d
Cucumber090-1 <gavroleg7@gmail.com>
2025-03-04 18:33:35 +0300

    updates

--- Course2/AlgoDataStructures/main.cpp
+++ Course2/AlgoDataStructures/main.cpp
@@ -1,11 +1,41 @@
 #include <iostream>
+#include <map>
+#include <set>
+#include <stack>
+#include <vector>
 
 using namespace std;
+
+struct Scope{
+    map<string, stack<int>> vars;
+};
+
 void solve() {
+    stack<Scope> scopes;
+    scopes.emplace();
+    string line;
 
-    
-
+    while (cin >> line) {
+        if (line.find('{') != string::npos) {
+            scopes.emplace();
+        } else if (line.find('}') != string::npos) {
+            scopes.pop();
+        } else {
+            string variable = line.substr(0, line.find('='));
+            string value = line.substr(line.find('=') + 1);
+            int number;
+            if (!value.empty() && (isdigit(value[0]) || value[0] == '-')) number = stoi(value);
+            else {
+                map<string, stack<int>> current = scopes.top().vars;
+                if (current.find(value) != current.end() && !current[value].empty()) number = current[value].top();
+                else number = 0;
+                cout << number << '\n';
+            }
+            scopes.top().vars[variable].push(number);
+        }
+    }
 }
+
 int main(){
     ios::sync_with_stdio(false); cin.tie(NULL);
     int t = 1;
--- Course2/Physics/Task2/lastNameHeating/lastNameCharacter.pde
+++ Course2/Physics/Task2/lastNameHeating/lastNameCharacter.pde
@@ -7,8 +7,9 @@ start(0,0)
   line to (2,3)   natural (T)=0
   line to (2,2)   natural (T)=0
   line to (1,2)   natural (T)=0
-  line to (1,0)   natural (T)=0
-  line to (0,0)   natural (T)=0
+  line to (1,0)   value (T)=0
+  line to (0,0)   
+  
 PLOTS CONTOUR(T) 
 PLOTS CONTOUR(100*exp( -(x-0.75)^2 - (y-1.75)^2)) 
 VECTOR(-grad(T)) END

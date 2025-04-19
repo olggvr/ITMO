7663f383484e20b04cf1c27dbd02fe345fa82524
Cucumber090-1 <gavroleg7@gmail.com>
2025-04-19 15:54:58 +0300

    solve l task

---
+++ Course2/AlgoDataStructures/block_3/main.cpp
@@ -0,0 +1,16 @@
+#include <deque>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+void solve() {
+    
+
+}
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    solve();
+}
